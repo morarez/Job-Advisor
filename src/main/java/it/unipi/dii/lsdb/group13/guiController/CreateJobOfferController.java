2609b7fb60b11dec8baa6fe0e42ff5ad0146ceba
EricaDallatomasina <erica.dallatomasina@gmail.com>
2021-02-03 18:23:06 +0100

    Added confirmation page after the creation of job offer

--- src/main/java/it/unipi/dii/lsdb/group13/guiController/CreateJobOfferController.java
+++ src/main/java/it/unipi/dii/lsdb/group13/guiController/CreateJobOfferController.java
@@ -1,16 +1,17 @@
 package it.unipi.dii.lsdb.group13.guiController;
 
 import it.unipi.dii.lsdb.group13.App;
-import it.unipi.dii.lsdb.group13.database.EmployerDao;
 import it.unipi.dii.lsdb.group13.database.JobOfferDao;
 import it.unipi.dii.lsdb.group13.entities.JobOffer;
 import it.unipi.dii.lsdb.group13.main.Session;
 import javafx.fxml.FXML;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
-import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
 
 import java.io.IOException;
 
@@ -47,7 +48,8 @@ public class CreateJobOfferController {
     public void pressSubmit() throws IOException{
         JobOfferDao jobOfferDao = new JobOfferDao();
         Session.getSingleton();
-        String companyName =Session.getLoggedUser();
+        String companyName = Session.getLoggedUser();
+
         if(titleField.getText().isEmpty() || stateField.getText().isEmpty() || cityField.getText().isEmpty() ||
                 jobTypeField.getValue() == null || descriptionField.getText().isEmpty() ){
             errorMsg.setText("Please enter all the required Fields!");
@@ -59,18 +61,34 @@ public class CreateJobOfferController {
                     salaryFromField.getText(),salaryToField.getText(), salaryUnit.getValue().toString());
             if (!jobOfferDao.createNewJobOffer(jobOffer))
                 errorMsg.setText("Something went wrong! please try again!");
-            else
-                //Erica please fix this :)
-                App.setRoot("JobCreationConfirmation");
+            else {
+                loadConfirmationPage();
+                App.setRoot("EmployerMenu");
+                //App.setRoot("JobCreationConfirmation");
+            }
         }  else{
             // When user DOES NOT fill the required fields for salary
             JobOffer jobOffer = new JobOffer(titleField.getText(),companyName,descriptionField.getText()
                     ,jobTypeField.getValue().toString(),stateField.getText(),cityField.getText());
             if (!jobOfferDao.createNewJobOffer(jobOffer))
                 errorMsg.setText("Something went wrong! please try again!");
-            else
-                //Erica please fix this :)
-                App.setRoot("JobCreationConfirmation");
+            else {
+                loadConfirmationPage();
+                App.setRoot("EmployerMenu");
+                //App.setRoot("JobCreationConfirmation");
+            }
         }
     }
+
+    private void loadConfirmationPage() throws IOException {
+        Label confirmMsg = new Label("JOB OFFER HAS BEEN CREATED SUCCESSFULLY!");
+        confirmMsg.setStyle("-fx-font-weight: bold ; -fx-font-size: 18");
+        confirmMsg.setPadding(new Insets(15));
+        Stage confirmation = new Stage();
+        confirmation.setTitle("---- Confirmation window ----");
+        confirmation.setScene(new Scene(confirmMsg, 440, 100));
+        confirmation.show();
+    }
+
+
 }
