19d188444d6921589fa3f8952de6181b46dacaa8
m-arezoumandan <mortezaarezoomandan@gmail.com>
2021-02-03 21:07:00 +0330

    Fixed an issue with creating job offer

--- src/main/java/it/unipi/dii/lsdb/group13/database/JobOfferDao.java
+++ src/main/java/it/unipi/dii/lsdb/group13/database/JobOfferDao.java
@@ -10,16 +10,24 @@ public class JobOfferDao {
 
     public boolean createNewJobOffer(JobOffer jobOffer){
         boolean ret = true;
+        Document job;
         try {
             MongoDBManager mongoDB = MongoDBManager.getInstance();
             Document locationDoc = new Document("city", jobOffer.getLocation().getCity())
                     .append("state", jobOffer.getLocation().getState());
-            Document salaryDoc = new Document("from", jobOffer.getSalary().getFrom())
+            if(jobOffer.getSalary() != null){
+                Document salaryDoc = new Document("from", jobOffer.getSalary().getFrom())
                     .append("to", jobOffer.getSalary().getTo()).append("time_unit",jobOffer.getSalary().getTimeUnit());
-            Document job = new Document("_id", jobOffer.getId()).append("job_title", jobOffer.getTitle())
+                job = new Document("_id", jobOffer.getId()).append("job_title", jobOffer.getTitle())
                     .append("company_name", jobOffer.getCompanyName()).append("location", locationDoc)
                     .append("salary", salaryDoc).append("post_date",jobOffer.getPostDate())
                     .append("job_type",jobOffer.getJobType()).append("job_description",jobOffer.getDescription());
+            }else {
+                job = new Document("_id", jobOffer.getId()).append("job_title", jobOffer.getTitle())
+                        .append("company_name", jobOffer.getCompanyName()).append("location", locationDoc)
+                        .append("post_date",jobOffer.getPostDate()).append("job_type",jobOffer.getJobType())
+                        .append("job_description",jobOffer.getDescription());
+            }
             mongoDB.getJobOffersCollection().insertOne(job);
             // Add the job offer to the list of company's job offers
             mongoDB.getCompaniesCollection().updateOne(eq("_id", jobOffer.getCompanyName()),
