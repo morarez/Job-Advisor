b1152abc185c149c3b18fa923d6ab415322ca942
EricaDallatomasina <erica.dallatomasina@gmail.com>
2021-02-15 15:26:16 +0100

    Deleted an uneseful import

--- src/main/java/it/unipi/dii/lsdb/group13/guiController/FollowedCompaniesController.java
+++ src/main/java/it/unipi/dii/lsdb/group13/guiController/FollowedCompaniesController.java
@@ -7,7 +7,6 @@ import javafx.beans.property.ReadOnlyObjectWrapper;
 import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
